a72945ec8046f1f2b128676aa27c0099086bc238
Anoop810 <Anoopsonawane810@gmail.com>
2026-06-18 16:17:13 +0530

    env updates

--- complaint/build.sh
+++ complaint/build.sh
@@ -3,3 +3,7 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py migrate --noinput
+
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
+  python manage.py createsuperuser --noinput 2>/dev/null || true
+fi
